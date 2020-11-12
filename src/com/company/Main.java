2276ea6cbae4c093dc0e0a2ed82b1fb7981da7dc
Viktoriia2004 <vshishulina@mail.ru>
2020-11-12 10:44:46 -0500

    workin on mess1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello gIt last day");
+
+        //B21 is exited
+        //
+
+
+
+
+
     }
 }
